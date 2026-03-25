518922f748a7c23e407b5c060bc4beb946f374c6
MAC <aminasow98@gmail.com>
2026-03-25 17:37:25 +0000

    changement du favicon

---
+++ app/head.ts
@@ -0,0 +1,15 @@
+export default defineAppHead({
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/assets/logo.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      type: "image/png",
+      href: "/assets/logo.png",
+    },
+  ],
+})
+
